93faad3e9a849eabd05c2b5ace383d04184622dc
Jonathan Mallefroy <jonathan.mallefroy@gmail.com>
2025-03-11 19:09:44 +0100

    add some files

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
 import "./Main.css";
+import "./card.jsx";
 
 function Main() {
   return (
     <div className='main'>
         <img className='banner' src="src\assets\Banner.png" alt="Banner" />
+        <div className='section'>
+          <card/>
+          <card/>
+          <card/>
+          <card/>
+          <card/>
+          <card/>
+        </div>
     </div>
   )
 }
---
+++ src/components/card.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./card.css"
+
+function card() {
+  return (
+    <div className='card'>
+        <p>Card</p>
+    </div>
+  )
+}
+
+export default card
