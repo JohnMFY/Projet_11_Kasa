de903d726d731d2174701eba5589e869cc326bea
Jonathan Mallefroy <jonathan.mallefroy@gmail.com>
2025-03-16 19:25:41 +0100

    ROUTER link active DONE

--- src/main.jsx
+++ src/main.jsx
@@ -5,10 +5,11 @@ import App from './App.jsx'
 
 const root = document.getElementById("root");
 
+
 ReactDOM.createRoot(root).render(
   <BrowserRouter>
     <Routes>
-      <Route path="/" element={<App />} errorElement={<h1>TEST ERROR</h1>}/>
+      <Route path="/" element={<App />}/>
       <Route path="/about" element={<div>A propos</div>}/>
       <Route path="/appartement" element={<div>Appartement</div>}/>
       <Route path="/*"  element={<div>Error 404</div>}/>
