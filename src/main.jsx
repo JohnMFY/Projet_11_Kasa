29d1ca6f131fe8ee2c62ddf068ff17ee7e2d6d8e
Jonathan Mallefroy <jonathan.mallefroy@gmail.com>
2025-03-16 18:36:35 +0100

    WIP ROUTER

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router";
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const root = document.getElementById("root");
+
+ReactDOM.createRoot(root).render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="/about" element={<div>A propos</div>} />
+      <Route path="/appartement" element={<div>Appartement</div>} />
+    </Routes>
+  </BrowserRouter>
+);
