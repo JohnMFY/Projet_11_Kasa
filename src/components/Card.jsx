5eef4b57957e8a48bff787df88ed6b6645c8cb7a
Jonathan Mallefroy <jonathan.mallefroy@gmail.com>
2025-03-17 18:45:08 +0100

    WIP use of datas

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,12 +1,59 @@
 import React from 'react'
 import "./Card.css"
 import { NavLink } from "react-router";
+import appartementsData from "../data.json";
 
+const appartements = appartementsData
+ appartements.map((appartement) =>
+  console.log(appartement)
+);
+
+function appartementCard(appartementsData){
+  const appartements = appartementsData
+  const card = (
+    <div className='card'>
+      {appartements.map((appartement)=>
+        <h3>
+          {appartement.title}
+        </h3>
+      )}
+    </div>
+  );
+  return card
+}
+console.log(appartementCard)
+/**** Documentation REACT ****/
+/*
+function Blog(props) {
+  const sidebar = (
+    <ul>
+      {props.posts.map((post) =>
+        <li key={post.id}>
+          {post.title}
+        </li>
+      )}
+    </ul>
+  );
+  const content = props.posts.map((post) =>
+    <div key={post.id}>
+      <h3>{post.title}</h3>
+      <p>{post.content}</p>
+    </div>
+  );
+  return (
+    <div>
+      {sidebar}
+      <hr />
+      {content}
+    </div>
+  );
+}
+*/
 function Card() {
   return (
     <div className='card'>
         <NavLink to={"/appartement"}>
-        Titre de la location
+        appartements.title
         </NavLink>
     </div>
   )
--- src/main.jsx
+++ src/main.jsx
@@ -2,17 +2,18 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router";
 import App from './App.jsx'
+import Nav from "./components/Nav.jsx";
+
 
 const root = document.getElementById("root");
-
 
 ReactDOM.createRoot(root).render(
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<App />}/>
-      <Route path="/about" element={<div>A propos</div>}/>
-      <Route path="/appartement" element={<div>Appartement</div>}/>
-      <Route path="/*"  element={<div>Error 404</div>}/>
+      <Route path="/about" element={<Nav/>}/>
+      <Route path="/appartement" element={<Nav/>}/>
+      <Route path="*"  element={<h1>Error 404</h1>}/>
     </Routes>
   </BrowserRouter>
 );
