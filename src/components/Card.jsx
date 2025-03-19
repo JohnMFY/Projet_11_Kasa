4a9c5a4183d5b972ded0f84e8b6802e0982f75e5
Jonathan Mallefroy <jonathan.mallefroy@gmail.com>
2025-03-19 08:20:01 +0100

    Affichage dynamique des locations sur homepage

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,59 +1,13 @@
 import React from 'react'
 import "./Card.css"
 import { NavLink } from "react-router";
-import appartementsData from "../data.json";
 
-const appartements = appartementsData
- appartements.map((appartement) =>
-  console.log(appartement)
-);
-
-function appartementCard(appartementsData){
-  const appartements = appartementsData
-  const card = (
-    <div className='card'>
-      {appartements.map((appartement)=>
-        <h3>
-          {appartement.title}
-        </h3>
-      )}
-    </div>
-  );
-  return card
-}
-console.log(appartementCard)
-/**** Documentation REACT ****/
-/*
-function Blog(props) {
-  const sidebar = (
-    <ul>
-      {props.posts.map((post) =>
-        <li key={post.id}>
-          {post.title}
-        </li>
-      )}
-    </ul>
-  );
-  const content = props.posts.map((post) =>
-    <div key={post.id}>
-      <h3>{post.title}</h3>
-      <p>{post.content}</p>
-    </div>
-  );
-  return (
-    <div>
-      {sidebar}
-      <hr />
-      {content}
-    </div>
-  );
-}
-*/
-function Card() {
+function Card(props) {
   return (
     <div className='card'>
-        <NavLink to={"/appartement"}>
-        appartements.title
+        <NavLink to={"/appartement"} state={{id : props.id}}>
+        <img src={props.image} alt="" />
+        <div className="appartementTitle">{props.title}</div>
         </NavLink>
     </div>
   )
--- src/components/SectionLocations.jsx
+++ src/components/SectionLocations.jsx
@@ -1,16 +1,23 @@
 import React from 'react'
 import "./SectionLocations.css"
 import Card from './card'
+import appartementsData from "../data.json";
+
 
 function SectionLocations() {
+  const appartements = appartementsData
+    appartements.map((appartement) =>
+    console.log(appartement)
+  );
   return (
     <div className='sectionLocations'>
-      <Card/>
-      <Card/>
-      <Card/>
-      <Card/>
-      <Card/>
-      <Card/>
+      {appartements.map((appartement)=>
+        <Card
+          id=     {appartement.id}
+          title=  {appartement.title}
+          image=  {appartement.cover}
+        />
+      )}
     </div>
   )
 }
