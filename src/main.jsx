4fc562775fb150e0cc63a863dcc04834998fe5ae
Jonathan Mallefroy <jonathan.mallefroy@gmail.com>
2025-03-27 19:16:11 +0100

    Pages files

--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,30 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route} from "react-router";
-import App from './App.jsx'
-import Nav from "./components/Nav.jsx";
-
+import Nav from './components/Nav';
+import Footer from './components/Footer';
+import Home from "./pages/Home";
+import About from "./Pages/About.jsx"
+import Flat from "./Pages/Flat.jsx"
+import Error from "./Pages/Error.jsx"
 
 const root = document.getElementById("root");
+
 
 ReactDOM.createRoot(root).render(
   <BrowserRouter>
     <Routes>
-      <Route path="/" element={<App />}/>
-      <Route path="/about" element={<Nav/>}/>
-      <Route path="/appartement" element={<Nav/>}/>
-      <Route path="*"  element={<h1>Error 404</h1>}/>
+      
+        <Route path="/" element={<Home/>}/>
+        <Route path="/about" element={<Nav/>}/>
+        <Route path="/appartement" element={<Nav/>}/>
+        <Route path="*"  element={<Error/>}/>
+   
     </Routes>
   </BrowserRouter>
 );
+
+/*
+<Route path="/" element={<layout/>}>
+</Route>  
+*/
---
+++ src/pages/Home.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import Nav from '../components/Nav';
+import Main from '../components/Main';
+import Footer from '../components/Footer';
+
+import "./Home.scss";
+
+function home() {
+  return (
+        <div>
+            <Nav/>
+            <Main/>
+            <Footer/>
+        </div>
+    )
+}
+
+export default home
