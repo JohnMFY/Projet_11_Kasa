a4aef9ac6cfbf7b51d534386eeec56f486dbbfb1
Jonathan Mallefroy <jonathan.mallefroy@gmail.com>
2025-04-11 00:44:14 +0200

    FlatInfo DONE | TODO stars dynamisation

--- src/components/FlatInfos.jsx
+++ src/components/FlatInfos.jsx
@@ -2,24 +2,30 @@ import React from 'react'
 import "./FlatInfos.scss"
 
 function FlatInfos(props) {
+  const tags = props.tags;
+  console.log(tags)
   return (
-    <div>
-        <div>
-            <h2>{props.title}</h2>
-            <p>{props.location}</p>
-            <div>{props.tags}</div>
+    <div className='flatInfo'>
+        <div className='flatDivLeft'>
+            <h1 className='flatTitle'>{props.title}</h1>
+            <p className='flatLocation'>{props.location}</p>
+            <div className='tagsDiv'>
+              {tags.map((tag)=><p className='flatTag'>{tag}</p>)}
+            </div>
         </div>
         <div>
-            <div>
-            <p>{props.hostName}</p>
-            <img src={props.hostPicture} alt="profil picture" />
-            </div>
-            <div>
-              <i className="fa-solid fa-star star"></i>
-              <i className="fa-solid fa-star star"></i>
-              <i className="fa-solid fa-star star"></i>
-              <i className="fa-solid fa-star star"></i>
-              <i className="fa-solid fa-star"></i> 
+            <div className='flatDivRight'>
+              <div className='hostDiv'>
+                <p className='hostName'>{props.hostName}</p>
+                <img src={props.hostPicture} alt="profil picture" />
+              </div>
+              <div className='stars'>
+                <i className="fa-solid fa-star fa-xl star"></i>
+                <i className="fa-solid fa-star fa-xl star"></i>
+                <i className="fa-solid fa-star fa-xl star"></i>
+                <i className="fa-solid fa-star fa-xl star"></i>
+                <i className="fa-solid fa-star fa-xl"></i>
+              </div>
             </div>
         </div>
     </div>
