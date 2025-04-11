5f13e392e0dd762c2da215818666f24e838279d9
Jonathan Mallefroy <jonathan.mallefroy@gmail.com>
2025-04-11 11:55:01 +0200

    DONE Carousel

--- src/components/FlatCarousel.jsx
+++ src/components/FlatCarousel.jsx
@@ -1,19 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./FlatCarousel.scss"
 
 function FlatCarousel(props) {
   const pictures = props.pictures;
+  const [pictureIndex, setPictureIndex] = useState(0)
+  const previous = () =>{
+    setPictureIndex(index => {
+      if(index === 0 ) return pictures.length -1
+      return index -1
+    })
+  }
+  const next = () =>{
+    setPictureIndex(index => {
+      if(index === pictures.length -1) return 0
+      return index +1
+    })
+  }
   return (
     <div className='Carousel'>
-      {pictures.map((picture)=>
-      <img src={picture}></img>
-      )}
+      <img src={pictures[pictureIndex]}/>
       <div className='selection'>
-      <i className="fa-solid fa-chevron-left fa-7x"></i>
-      <i className="fa-solid fa-chevron-right fa-7x"></i>
+      <i className="fa-solid fa-chevron-left fa-7x" onClick={previous}></i>
+      <i className="fa-solid fa-chevron-right fa-7x" onClick={next}></i>
       </div>
     </div>
   )
 }
-
 export default FlatCarousel
