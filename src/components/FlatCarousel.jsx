836a3e96e60a4c564f9f8ecfeba71c3aafaaf1da
Jonathan Mallefroy <jonathan.mallefroy@gmail.com>
2025-05-09 19:15:13 +0200

    Finalization

--- src/components/FlatCarousel.jsx
+++ src/components/FlatCarousel.jsx
@@ -17,13 +17,15 @@ function FlatCarousel(props) {
       return index +1
     })
   }
+
   return (
     <div className='Carousel'>
       <img src={pictures[pictureIndex]}/>
       <div className='selection'>
-      <i className="fa-solid fa-chevron-left fa-7x" onClick={previous}></i>
-      <i className="fa-solid fa-chevron-right fa-7x" onClick={next}></i>
+        <i className="fa-solid fa-chevron-left fa-7x" onClick={previous}></i>
+        <i className="fa-solid fa-chevron-right fa-7x" onClick={next}></i>
       </div>
+      <p className='picturesCount'>{pictureIndex+1}/{pictures.length}</p>
     </div>
   )
 }
